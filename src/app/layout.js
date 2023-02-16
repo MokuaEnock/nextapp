c2bdb5556a2e24b2a7c4ff4a00dc157b25a5a3bb
Enock <enock.mokua@student.moringaschool.com>
2023-02-16 08:59:31 +0300

    add footer component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Footer from "./footer";
 
 export default function RootLayout({ children }) {
   return (
@@ -9,6 +10,7 @@ export default function RootLayout({ children }) {
       */}
       <head />
       <body>{children}</body>
+      <Footer />
     </html>
   );
 }
